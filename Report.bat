e8706b4930795628df895fe2c2d32d07a5b6f2b1
gd0810 <zeefer2000@gmail.com>
2026-04-04 14:50:05 +0530

    update:tailwind_cdn_bug

--- Report.bat
+++ Report.bat
@@ -10,7 +10,7 @@ REM Start Flask app in background
 start cmd /k python app.py
 
 REM Wait for server to start
-timeout /t 3 >nul
+timeout /t 6 >nul
 
 REM Open browser
 start http://127.0.0.1:5000/
